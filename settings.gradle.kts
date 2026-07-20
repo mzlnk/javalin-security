1bfe1e8a5242c5e140d812e617fae72cb87a187e
Marcin Zielonka <zielonka.marcin@protonmail.com>
2026-07-20 11:02:23 +0200

    🔨 aligned gradle root project name

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
 }
 
-rootProject.name = "javalin-security"
+rootProject.name = "javalin-security-root"
 
 include("core")
 project(":core").name = "javalin-security"
